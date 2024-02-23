a2ab939d8bd3f0f9c97f2feb55e488d2961403ab
jpareja <jpareja@D2060>
2024-02-23 12:39:04 -0500

    Terminada sin funciones completas

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,18 +1,23 @@
-﻿namespace Consoleapp1
+﻿using System.Runtime.CompilerServices;
+
+namespace Consoleapp1
 {
     static class Program
     {
         static void Main()
         {
             Console.WriteLine("Fruteria UNAD, Peras, Manzanas, Ciruelas, Bananas, Piñas.");
-            Console.WriteLine("Ingrese el numero de Manzanas que desea llevar");
-            int manzanas = Convert.ToInt32(Console.ReadLine());
+            
+            ///
+                Console.WriteLine("Ingrese el numero de Manzanas que desea llevar");
+                int manzanas = Convert.ToInt32(Console.ReadLine());
 
-            const int manzanas_value = 560;
-            int manzanas_total = manzanas * manzanas_value; 
+                const int manzanas_value = 560;
+                int manzanas_total = manzanas * manzanas_value;
 
-            Console.WriteLine("Ha ordenado " + manzanas + " Manzanas");
-            Console.WriteLine("Por un valor total de " + manzanas_total + " Pesos");
+                Console.WriteLine("Ha ordenado " + manzanas + " Manzanas");
+                Console.WriteLine("Por un valor total de " + manzanas_total + " Pesos");
+
             // Manzanas 
 
             Console.WriteLine("Ingrese el numero de peras que desea llevar");
@@ -48,13 +53,29 @@
             Console.WriteLine("Ingrese el numero de piñass que desea llevar");
             int piñas = Convert.ToInt32(Console.ReadLine());
 
-            const int manzanas_value = 560;
-            int manzanas_total = manzanas * manzanas_value;
+            const int piñas_value = 560;
+            int piñas_total = piñas * piñas_value;
 
-            Console.WriteLine("Ha ordenado " + manzanas + " Manzanas");
-            Console.WriteLine("Por un valor total de " + manzanas_total + " Pesos");
+            Console.WriteLine("Ha ordenado " + piñas + " Piñas");
+            Console.WriteLine("Por un valor total de " + piñas_total + " Pesos");
+            //Piñas
 
 
+            double order_value = piñas_total + manzanas_total + bananas_total + ciruelas_total + peras_total;
+
+
+            Console.WriteLine("Su orden ha finalizado, el valor total a pagar es " + order_value + " pesos");
+            void discount()
+            {
+                if (order_value >= 20000)
+                {
+                    order_value = order_value * 0.90;
+                    Console.WriteLine("Su descuento efectuado fue del 10% por sobrepasar una compra de $20000, el valor final a pagar es de " + order_value);
+                }
+            }
+
+            discount();
+            
             /*
            Console.WriteLine("Ingrese el numero de Ciruelas que desea llevar");
            Console.WriteLine("Ingrese el numero de Bananas que desea llevar");
